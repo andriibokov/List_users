bc92dc08f329d7b2e69aaa4fe95a55336bfdf20a
Andrii Bokov <bokov579@gmail.com>
2020-11-03 15:36:25 +0200

    итого 9 часов

--- js/main.js
+++ js/main.js
@@ -1,24 +1,108 @@
-(async function Users(){
+let usersList
+let massage = ''
+let userSearchList = []
+let checkBox = true
+
+async function Users(indexRemove, inputValue){
     try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-        const users = await response.json()
-        const user = users.map(user => 
-        `<li class="user__item">
-            <span class="user__info">${user.name}</span>
-            <span class="user__info">${user.email}</span>
-            <span class="user__info">${user.company.name}</span>
-            <span class="user__info">${user.address.city}</span>
-            <button class="user__remove" onclick="userRemove()">
-                <img src="./image/user_remove.svg" alt="image user remove">
-            </button>
-        </li>
-        `)
-        document.querySelector('#users').innerHTML = user.join("");
+        if(checkBox){
+
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            usersList = await response.json()
+            usersList = usersList.map(user => {
+                return user = {
+                   name: user.name,
+                   email: user.email,
+                   companyName: user.company.name,
+                   addressCity: user.address.city
+                }
+            })
+            checkBox = false
+            renderUserList (usersList)
+        } else {     /* Удаление User */ 
+
+            usersList = usersList.filter((user, index) => index != indexRemove)
+            renderUserList (usersList)
+        }
+
+        if(inputValue){    /* Поиск по Users */ 
+            userSearchList = usersList.filter(user =>{
+                if(Object.values(user).join('').toUpperCase().indexOf(inputValue) > -1) return true
+            })
+            renderUserList (userSearchList)
+        }
     } catch (e) {
+
         console.error(e);
+
+    } finally {
+        if(usersList.length === 0 || userSearchList.length === 0 && inputValue){
+            massage = 'User list is empty.'
+            plugUserList(massage)
+        }
     }
-}())
+} 
+
+Users()
+
+/*Заглушка */
+function plugUserList(massage){
+    document.querySelector('#users').innerHTML = `<div class="list__empty">${massage}</div>`
+}
+
+/* удаление User */ 
+function userRemove(elementValue){
+    Users(elementValue)
+}
+
+/* Поиск по Users */
+function search(value){
+    value = value.toUpperCase()
+    Users(-1, value)
+}
+
+/* Воспроизведение Users */
+function renderUserList (usersList){
+    const users = usersList.map((user,index) => 
+    `<li class="user__item">
+        <span class="user__info">${user.name}</span>
+        <span class="user__info">${user.email}</span>
+        <span class="user__info">${user.companyName}</span>
+        <span class="user__info">${user.addressCity}</span>
+        <button class="user__remove" value="${index}" onclick="userRemove(this.value)">
+            <img src="./image/user_remove.svg" alt="image user remove">
+        </button>
+    </li>
+`)
+document.querySelector('#users').innerHTML = users.join("");
+}
 
 
+/* Стилизация при наведении на User */ 
+
+let currentElem = null;
+
+users.onmouseover = function(event) {
+
+    if (currentElem) return;
+    let target = event.target.closest('.user__item');
+    
+    if (!target) return;    
+
+    currentElem = target;
+    target.classList.add('active');
+    target.lastElementChild.style.visibility = 'visible'
+};
+  
+users.onmouseout = function(event) {
+    if (!currentElem) return;
+    let relatedTarget = event.relatedTarget;
+    
+    if (relatedTarget === currentElem) return;
+
+    currentElem.classList.remove('active');
+    currentElem.lastElementChild.style.visibility = 'hidden'
+    currentElem = null;
+};
 
 
